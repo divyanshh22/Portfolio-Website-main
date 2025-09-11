b505c54b8e4332a49c405b0cce6eab10048b1e49
Divyansh Singh <129421803+divyanshh22@users.noreply.github.com>
2025-09-11 05:45:11 +0530

    Update script.js

--- projects/script.js
+++ projects/script.js
@@ -1,88 +1,69 @@
 $(document).ready(function () {
-    $('#menu').click(function () {
-        $(this).toggleClass('fa-times');
-        $('.navbar').toggleClass('nav-toggle');
-    });
+  $('#menu').click(function () {
+    $(this).toggleClass('fa-times');
+    $('.navbar').toggleClass('nav-toggle');
+  });
 
-    $(window).on('scroll load', function () {
-        $('#menu').removeClass('fa-times');
-        $('.navbar').removeClass('nav-toggle');
-
-        if (window.scrollY > 60) {
-            document.querySelector('#scroll-top').classList.add('active');
-        } else {
-            document.querySelector('#scroll-top').classList.remove('active');
-        }
-    });
+  $(window).on('scroll load', function () {
+    $('#menu').removeClass('fa-times');
+    $('.navbar').removeClass('nav-toggle');
+    $('#scroll-top').toggleClass('active', window.scrollY > 60);
+  });
 });
 
 document.addEventListener('visibilitychange', function () {
-    if (document.visibilityState === "visible") {
-        document.title = "Projects | Portfolio Divyansh Singh";
-        $("#favicon").attr("href", "../assets/images/favicon.png");
-    } else {
-        document.title = "Come Back To Portfolio";
-        $("#favicon").attr("href", "../assets/images/favhand.png");
-    }
+  if (document.visibilityState === "visible") {
+    document.title = "Projects | Portfolio Divyansh Singh";
+    $("#favicon").attr("href", "../assets/images/favicon.png");
+  } else {
+    document.title = "Come Back To Portfolio";
+    $("#favicon").attr("href", "../assets/images/favhand.png");
+  }
 });
 
-// fetch projects start
 function getProjects() {
-    // FIXED path
-    return fetch("./projects.json")
-        .then(response => response.json())
-        .then(data => data);
+  return fetch("./projects.json").then(res => res.json());
 }
 
 function showProjects(projects) {
-    let projectsContainer = document.querySelector(".work .box-container");
-    let projectsHTML = "";
-
-    projects.forEach(project => {
-        projectsHTML += `
-        <div class="grid-item ${project.category}">
-            <div class="box tilt" style="width: 380px; margin: 1rem">
-                <img draggable="false" src="../assets/images/projects/${project.image}.png" alt="project" />
-                <div class="content">
-                    <div class="tag"><h3>${project.name}</h3></div>
-                    <div class="desc">
-                        <p>${project.desc}</p>
-                        <div class="btns">
-                            <a href="${project.links.view}" class="btn" target="_blank"><i class="fas fa-eye"></i> View</a>
-                            <a href="${project.links.code}" class="btn" target="_blank">Code <i class="fas fa-code"></i></a>
-                        </div>
-                    </div>
-                </div>
+  const container = document.querySelector(".work .box-container");
+  container.innerHTML = projects.map(p => `
+    <div class="grid-item ${p.category}">
+      <div class="box tilt">
+        <img src="../assets/images/projects/${p.image}.png" alt="${p.name}" draggable="false">
+        <div class="content">
+          <div class="tag"><h3>${p.name}</h3></div>
+          <div class="desc">
+            <p>${p.desc}</p>
+            <div class="btns">
+              <a href="${p.links.view}" class="btn" target="_blank"><i class="fas fa-eye"></i> View</a>
+              <a href="${p.links.code}" class="btn" target="_blank">Code <i class="fas fa-code"></i></a>
             </div>
-        </div>`;
-    });
+          </div>
+        </div>
+      </div>
+    </div>
+  `).join('');
 
-    projectsContainer.innerHTML = projectsHTML;
-
-    // isotope filter
-    var $grid = $('.box-container').isotope({
-        itemSelector: '.grid-item',
-        layoutMode: 'fitRows'
-    });
-
-    $('.button-group').on('click', 'button', function () {
-        $('.button-group').find('.is-checked').removeClass('is-checked');
-        $(this).addClass('is-checked');
-        var filterValue = $(this).attr('data-filter');
-        $grid.isotope({ filter: filterValue });
-    });
+  const $grid = $('.box-container').isotope({
+    itemSelector: '.grid-item',
+    layoutMode: 'fitRows'
+  });
+  $('.button-group').on('click', 'button', function () {
+    $('.is-checked').removeClass('is-checked');
+    $(this).addClass('is-checked');
+    $grid.isotope({ filter: $(this).attr('data-filter') });
+  });
 }
 
-getProjects().then(data => {
-    showProjects(data);
-});
-// fetch projects end
+getProjects().then(showProjects);
 
-// disable developer mode
 document.onkeydown = function (e) {
-    if (e.keyCode == 123) return false;
-    if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) return false;
-    if (e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) return false;
-    if (e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) return false;
-    if (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) return false;
-}
+  if (e.keyCode === 123 ||
+      (e.ctrlKey && e.shiftKey && ['I','C','J','U'].includes(String.fromCharCode(e.keyCode))) ||
+      (e.ctrlKey && e.keyCode === 'U'.charCodeAt(0))) {
+    return false;
+  }
+};
+
+
