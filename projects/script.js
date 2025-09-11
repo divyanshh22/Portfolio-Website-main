fd4fb3c4aa1d206b328904860a609da24424c914
Divyansh Singh <129421803+divyanshh22@users.noreply.github.com>
2025-09-12 05:27:41 +0530

    Update script.js

--- projects/script.js
+++ projects/script.js
@@ -20,7 +20,7 @@ $(document).ready(function () {
 document.addEventListener('visibilitychange',
     function () {
         if (document.visibilityState === "visible") {
-            document.title = "Projects | Portfolio Jigar Sable";
+            document.title = "Projects | Portfolio Divyansh Singh";
             $("#favicon").attr("href", "/assets/images/favicon.png");
         }
         else {
@@ -124,5 +124,6 @@ document.onkeydown = function (e) {
         return false;
     }
 }
+
 
 
